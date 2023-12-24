c0237bd47cd935a26a1592d3bd6e6dad634e4f55
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-12-24 23:13:25 +0530

    feat: configure TypeScript, Nodemon, and Express

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+// Boot express
+const app: Application = express();
+const port = 5000;
+// Application routing
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Hello from Sanjay AS" });
+});
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
