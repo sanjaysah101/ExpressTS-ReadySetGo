79485fbfa210a3a1784a07be8e270865df772d3f
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2023-12-30 04:04:27 +0530

    feat(logging): add pino library and jest tests

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, { type Application } from 'express';
 import dotenv from 'dotenv';
+import pinoHTTP from 'pino-http';
 
+import { logger } from './utils/Logger';
+
+// import { logger } from './utils/Logger';
 import { routes } from './routes';
 
 dotenv.config();
 
 // Boot express
 export const app: Application = express();
+
+app.use(pinoHTTP({ logger }));
 
 // Application routing
 routes(app);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { app } from './app';
+import { logger } from './utils/Logger';
 
 const port = process.env.APP_PORT ?? 3000;
 
 // Start server
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is listening on port ${port}!`);
+  logger.info(`Server is listening on port ${port}!`);
 });
